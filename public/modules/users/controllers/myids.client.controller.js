0e5ad079b16c641c61318986f2f712e96dbc3d28
pnganga <pnganga05@gmail.com>
2017-12-18 14:09:42 +0300

    changed route for user viewing my posts

--- public/modules/users/controllers/myids.client.controller.js
+++ public/modules/users/controllers/myids.client.controller.js
@@ -5,7 +5,7 @@ angular.module('users').controller('MyidsController', ['$scope', '$http', '$loca
         $scope.authentication = Authentication;
         if ($scope.authentication) {
             $scope.find = function() {
-                $http.get('/nationals/' + $scope.authentication.user.phoneNumber).success(function(res) {
+                $http.get('/nationalids/' + $scope.authentication.user.phoneNumber).success(function(res) {
                     $scope.ids = res;
                     $scope.alert = 'alert alert-danger';
                 }).error(function(res) {
