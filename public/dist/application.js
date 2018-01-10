ad86d7b58b781b4ca2dc3873cfabc42ce947f837
pnganga <pnganga05@gmail.com>
2018-01-11 00:12:55 +0300

    Added chat widget

--- public/dist/application.js
+++ public/dist/application.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'SakaDocs - Your document finding solution';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'ngPassword'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
@@ -2347,6 +2347,38 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$l
 			});
 		};
 	}
+]);
+'use strict';
+
+angular.module('users').directive('equals', [
+    function() {
+        return {
+            restrict: 'A', // only activate on element attribute
+            require: '?ngModel', // get a hold of NgModelController
+            link: function(scope, elem, attrs, ngModel) {
+                if (!ngModel) return; // do nothing if no ng-model
+
+                // watch own value and re-validate on change
+                scope.$watch(attrs.ngModel, function() {
+                    validate();
+                });
+
+                // observe the other value and re-validate on change
+                attrs.$observe('equals', function(val) {
+                    validate();
+                });
+
+                var validate = function() {
+                    // values
+                    var val1 = ngModel.$viewValue;
+                    var val2 = attrs.equals;
+
+                    // set validity
+                    ngModel.$setValidity('equals', !val1 || !val2 || val1 === val2);
+                };
+            }
+        }
+    }
 ]);
 'use strict';
 
