4ea1ac68c1a56117d81c4c17d26d36e4dede7cb6
pnganga <pnganga05@gmail.com>
2018-01-15 01:37:09 +0300

    Added mobile logo

--- public/dist/application.js
+++ public/dist/application.js
@@ -917,6 +917,7 @@ angular.module('nationals').controller('NationalsController', ['$scope', '$http'
                 $http.get('/nationalids').success(function(res) {
                     $scope.ids = res;
                     $scope.alert = 'alert alert-danger';
+                
                 }).error(function(res) {
                     $scope.error = res.message;
                 });
