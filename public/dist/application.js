4ae7cc8e10af2c131843cd55943474dafbdbaf0b
pnganga <pnganga05@gmail.com>
2018-01-25 11:01:24 +0300

    Fixed scroll to top on new view

--- public/dist/application.js
+++ public/dist/application.js
@@ -26,13 +26,18 @@ var ApplicationConfiguration = (function() {
 //Start by defining the main module and adding the module dependencies
 angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
 
+
 // Setting HTML5 Location Mode
 angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
 	function($locationProvider) {
 		$locationProvider.hashPrefix('!');
 	}
 ]);
-
+angular.module(ApplicationConfiguration.applicationModuleName).run(["$rootScope", "$anchorScroll" , function ($rootScope, $anchorScroll) {
+    $rootScope.$on("$locationChangeSuccess", function() {
+                $anchorScroll();
+    });
+}]);
 //Then define the init function for starting up the application
 angular.element(document).ready(function() {
 	//Fixing facebook bug with redirect
