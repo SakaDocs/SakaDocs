3c531a40a5b9cadd3499d1cc1f08ce69abc75c88
pnganga <pnganga05@gmail.com>
2018-02-02 17:10:52 +0300

    Added logic for Mpesa payment and sms sending

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -6,5 +6,5 @@ module.exports = function(app) {
 	app.route('/').get(core.index);
 	app.route('/doctype').get(core.doctype);
 	app.route('/mpesac2bvalidation').post(core.mpesac2bvalidation);
-	app.route('/mpesac2bconfirmation').post(core.mpesac2bvalidation);
+	app.route('/mpesac2bconfirmation').post(core.mpesac2bconfirmation);
 };
